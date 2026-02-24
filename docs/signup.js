b20e4a7fc17ee16ba95fe7b953a2603d588841ee
mmcb2029be06 <b2029mmcarandang@pshs.edu.ph>
2026-02-24 22:31:05 +0800

    3rd commit

--- docs/signup.js
+++ docs/signup.js
@@ -27,8 +27,12 @@ submitSignup.addEventListener("click", function(e) {
     password: password,
     data : []
   };
-  package = loadNotes().push(starter);
+  let notes = loadNotes()
+  package = notes.push(starter); // when signingup the recently created account becomes first in queue
   saveNotes(package);
+
+  //document.getElementById("pswrd").innerHTML = "";
+  //document.getElementById("user").innerHTML = "";
 });
 
 const submitLogin = document.getElementById('Login');
@@ -37,10 +41,24 @@ submitLogin.addEventListener("click", function(e) {
   const password = document.getElementById('pswrd').value.trim();
   const user = document.getElementById('user').value.trim();
 
-  for (i in loadNotes()){
-    if (loadnotes[i].user == user){
-      package = loadNotes().push(loadnotes[i]);
+  let notes = loadNotes();
+  for (i in notes){
+    if (notes[i].user == user){
+      package = notes.push(notes[i]); // when logging in the logged in account becomes cloned and pushed into the first in queue (although not that efficient)
       saveNotes(package);
     }
   }
+
+  //document.getElementById("pswrd").innerHTML = "";
+  //document.getElementById("user").innerHTML = "";
 });
+
+const submitLogout = document.getElementById('Logout');
+submitLogout.addEventListener("click", function(e) {
+      let notes = loadNotes();
+      package = notes.push(0);
+      saveNotes(package); // first in queue becomes 0, so that means that there is no account logged in
+
+      //document.getElementById("pswrd").innerHTML = "";
+      //document.getElementById("user").innerHTML = "";
+    });
